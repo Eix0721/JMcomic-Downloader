1d6a9ec590ee764659cc901d9e3f46f32fc2abeb
Eix0721 <2737757070@qq.com>
2025-10-01 13:41:18 +0800

    优化代码结构与逻辑 优化模块检测与错误处理能力，并提升其泛用性 优化模块下载成功/失败提示，可精准定位错误原因 添加本子下载成功/失败提示，禁漫车错误时不再会导致程序卡死 添加指令错误提示 添加pyYAML模块，未来用于下载配置功能 添加“检测”功能，可手动检测第三方模块是否导入成功（虽然没什么用）

--- main/JMdownloader.py
+++ main/JMdownloader.py
@@ -1,61 +1,91 @@
-import os,time
+import os,time,sys
+
+#测试模块下载功能用
+#os.system("pip uninstall -y -q jmcomic")
+#os.system("pip uninstall -y -q pyyaml")
 
 menuText = \
-    "\n\n----------菜单----------\n"+\
-    "1.菜单：显示此菜单栏\n2.下载：下载JMcomic漫画\n"+\
-    "3.设置：设置文件路径等\n4.关于：显示关于页面\n"+\
-    "5.退出：退出程序\n"+\
-    "**输入数字以使用相关功能\n------------------------\n\n"
-moduleLink = "https://github.com/hect0x7/JMComic-Crawler-Python"
+"""\n\n-------------菜单-------------
+1.菜单：显示此菜单页面
+2.下载：下载JMcomic漫画
+3.设置：设置文件路径等
+4.关于：显示关于页面
+5.检测：检测非内置模块是否导入
+6.退出：退出程序
+**输入数字以使用相关功能
+------------------------------\n\n"""
+jmLink = "https://github.com/hect0x7/JMComic-Crawler-Python"#JMcomic
+yamlLink = "https://github.com/yaml/pyyaml"#pyYAML
 
-#检查模块是否存在
-def checkModule (moduleName):
+
+def checkModule (moduleName,moduleLink,packName=None):
+    packName = packName or moduleName
     try:
-        module = __import__(moduleName)
-        return module
+        module = __import__(packName)  
+        print (f" {moduleName} 模块导入成功！") 
     except ModuleNotFoundError:
-        command = input ("未检测到\"JMcomic\"模块，将通过pip进行安装（y/n）：")
-        if command == "y":
-            print ("即将开始安装：")
+        command = input (f"\n未检测到 {moduleName} 模块，将通过pip进行安装（Y/n）：")
+        if command.lower() == "y":#pip安装
+            print ("即将开始安装......")
             time.sleep (0.5)
-            os.system ("{os.sys.executable} -m pip install jmcomic")
-            print ("\n\n安装结束，如未生效，请手动安装\"jmcomic\"模块\n"+\
-                f"（JMcomic模块项目地址：{moduleLink}\n{menuText}）")
-        else:
-            print ("请安装\"jmcomic\"模块后使用\n"+\
-                   f"（JMcomic模块项目地址：{moduleLink}）")
-            exit ()
+            successDnld = os.system (f"\"{sys.executable}\" -m pip install -q {moduleName}")
+        else:#不同意安装
+            print (f"请安装 {moduleName} 模块后使用\n"+\
+                   f"模块项目地址：{moduleLink}")
+            sys.exit (0)
+        if successDnld != 0:#pip安装失败
+            print (f"模块安装失败，请手动安装 {moduleName} 模块！\n"+\
+                   f"模块项目地址：{moduleLink}")
+            sys.exit (0)
+        try:
+            module =  __import__ (packName)
+        except ModuleNotFoundError:#动态导入失败
+            print (f"模块已安装但动态导入失败，请重启脚本或手动安装 {moduleName} 模块\n"+\
+                   f"模块项目地址：{moduleLink}")
+            sys.exit (0)
+        else:#导入成功
+            print (f" {moduleName} 模块导入成功！")
+            return module
+    
 
+print (f"\n\n欢迎使用 JMcomic Downloader\n")
+jm = checkModule ("jmcomic",jmLink)
+yaml = checkModule ("pyYAML",yamlLink,"yaml")
+print (f"{menuText}")
 
-
-
-print (f"\n\n欢迎使用 JMcomic Downloader\n{menuText}")
-jm = checkModule ("jmcomic")
 
 while True:
-    command = input("请输入数字代号：")
+    command = input("\n请输入数字代号：")
 
     if command == "1":
         print (menuText)
 
     elif command == "2":
-        jmID = input ("请输入禁漫车：")
-        #含空格则分割至列表
-        #if " " in jmID:
-        print ("下载即将开始...")
-        jm.download_album (jmID)
-        print ("下载完成！\n")
+        jmID = input ("请输入禁漫车：")#添加批量下载 空格分割jm号
+        print ("\n下载即将开始...")
+        try:
+            jm.download_album (jmID)
+        except:
+            print ("\n**请求的本子不存在或请求时发生错误！")
+        print ("\n下载结束！\n")
     
     elif command == "3":
         print("开发中...")
 
     elif command == "4":
         print("开发中...")
-    
+
     elif command == "5":
+        checkModule ("jmcomic",jmLink,)
+        checkModule ("pyYAML",yamlLink,"yaml")
+
+    elif command == "6":
         print("程序即将退出.")
-        time.sleep (1)
-        exit()
+        time.sleep (0.2)
+        sys.exit(1)
+    
+    else:
+        print (f"{menuText}\n指令不存在，请输入数字代号以使用相关功能！")
     
 
 
