29f235db4774c6417e5d00a22fb04d8ecc4a2bb6
Eix0721 <2737757070@qq.com>
2025-11-13 21:27:33 +0800

    feat
    删除 设置、菜单选项前的序号
    新增 退出设置的设置选项
    新增 设置不自动退出
    新增 下载漫画时提示日志已关闭
    新增 交互式命令行界面的主题选择功能
    修复 部分终端选项指针显示异常的问题
    修改 关于、设置、菜单部分文本

--- src/libs/core.py
+++ src/libs/core.py
@@ -1,24 +1,24 @@
-# pyright: ignore[reportPrivateImportUsage]
-
 import traceback
 import time
 import sys
 import re 
+import os
 
-from libs import jmcomic as jm
+from libs import global_style, jmcomic as jm
 from libs import yaml
 from libs import InquirerPy
-from libs.InquirerPy import inquirer
-from libs.text import text
+from libs.InquirerPy import inquirer,utils
+from libs.text import text, infurce_style
 
 
 
 success_initialize = False
 is_show_download_log = True
+global_style = InquirerPy.get_style (infurce_style.all_styles["默认风格"],style_override=True)
 
 
-def initialize () -> bool:
-    """检测配置文件；ver 为版本标识，exe(无需导入)/py"""
+
+def initialize () ->bool:
     try:
         print ("\n\n欢迎使用 JMcomic Downloader\n")
         is_initialized = True
@@ -28,17 +28,23 @@ def initialize () -> bool:
     return is_initialized
 
 
-def jmcomic_download() -> None:
+def jmcomic_download() ->None:
+    global is_show_download_log,global_style
     jm_ids = InquirerPy.inquirer.text(  # type: ignore
         message="请输入要下载的JMcomic车号（多个车号用空格分隔）：",
+        style = global_style,
+        pointer = ">"
     ).execute()
     if not re.fullmatch(r"(\d+\s*)+", jm_ids):
         print ("输入的格式有误，请输入仅包含数字的车号，多个车号用空格分隔！")
         return
     jm_ids = jm_ids.strip().split ()
-    is_permit = InquirerPy.inquirer.confirm(message=f"即将开始下载{jm_ids}，是否确认？",default=True).execute()  # type: ignore
+    is_permit = InquirerPy.inquirer.confirm(message=f"即将开始下载{jm_ids}，是否确认？",# type: ignore
+                                            default=True,style = global_style,pointer = ">").execute()  
     if not is_show_download_log:
+        print ("已关闭下载日志输出，若要开启，请重启程序")
         print ("下载任务已开始，请耐心等待...")
+
     if is_permit:
         start_time = time.time()
         try:
@@ -52,47 +58,81 @@ def jmcomic_download() -> None:
     else:
         print ("已取消下载任务。")
 
-def get_command(option) -> str:
-    command = "1"
+def set_style() ->None:
+    global global_style
+    is_keep_going = True
+    style_choice = "默认风格"
+    while is_keep_going:
+        try:
+            style_choices = list(infurce_style.all_styles.keys()) + ["取消"]
+            style_choice = InquirerPy.inquirer.select(  # type: ignore
+                message = "请选择界面风格：",
+                choices = style_choices,
+                default = style_choice,
+                style = global_style,
+                pointer = ">"
+            ).execute()
+            if style_choice == "取消":
+                is_keep_going = False
+                return
+            style_args = infurce_style.all_styles[style_choice]
+            global_style = InquirerPy.get_style (style_args,style_override=True)
+        except Exception as err:
+            print(f"切换样式时发生错误：{type(err).__name__}:{err}")
+            break
+
+
+def get_command(option) ->str:
+    global global_style
+    command = ""
     if option == "menu":
-        command = InquirerPy.inquirer.select(  # type: ignore
+        command = InquirerPy.inquirer.select(   # type: ignore
             message="\n请选择操作：",
             choices=[
-                "1. 详细菜单","2. 下载漫画","3. 设置选项",
-                "4. 关于项目","5. 退出程序"
+                "详细菜单","下载漫画","设置选项",
+                "切换主题","关于项目","退出程序"
             ],
+            style = global_style,
+            pointer = ">"
         ).execute()
     elif option == "settings":
         command = InquirerPy.inquirer.select(  # type: ignore
             message="请选择设置项：",
             choices=[
-                "1. 关闭下载日志输出",
+                "关闭下载日志输出","退出设置"
             ],
+            style = global_style,
+            pointer = ">"
         ).execute()
-    return command[0]
+    return command
 
 
-def execute_command(command: str) -> None:
-    if command == "1":
+def execute_command(command: str) ->None:
+    global is_show_download_log,global_style
+    if command == "详细菜单":
         print (text.menu)
-    elif command == "2":
+    elif command == "下载漫画":
         jmcomic_download ()
-    elif command == "3":
-        command = get_command("settings")
-        if command == "1":
-            jm.disable_jm_log ()
-            is_show_download_log = False
-            print ("已关闭下载日志输出，若要开启，请重启程序")
-        else:
-            print (f"指令不存在，输入“1”以查看菜单")
-    elif command == "4":
+    elif command == "设置选项":
+        is_in_settings = True
+        while is_in_settings:
+            command = get_command("settings")
+            if command == "关闭下载日志输出":
+                jm.disable_jm_log ()
+                is_show_download_log = False
+                print ("已关闭下载日志输出，若要开启，请重启程序")
+            if command == "退出设置":
+                is_in_settings = False
+            else:
+                print (f"指令不存在，输入“1”以查看菜单")
+    elif command == "切换主题":
+        set_style()
+    elif command == "关于项目":
         print (text.about)
-    elif command == "5":
+    elif command == "退出程序":
         print ("程序即将退出")
         time.sleep (0.2)
         sys.exit (0)
-    elif command == "0721":
-        raise RuntimeError ("这是一个测试异常！")
     else:
         print (f"指令不存在，输入“1”以查看菜单")
 
@@ -102,9 +142,7 @@ def main():
         input("回车以退出程序...")
         return  
     print (text.menu)
-
     while True:
-        
         try:
             command = get_command("menu")
             execute_command (command)
--- src/libs/text.py
+++ src/libs/text.py
@@ -1,4 +1,5 @@
 from ast import In
+from ctypes import pointer
 
 
 class link:
@@ -10,37 +11,93 @@ class link:
 
 class text:
     menu = f"""
-\n\n{"-" * 18}菜单{"-" * 18}
-1.菜单：显示此菜单页面
-2.下载：下载JMcomic漫画
-    ╰以空格分割多个禁漫车以批量下载
-3.设置：设置文件路径等（开发中）
-4.关于：显示关于页面
-5.退出：退出程序
-**输入数字以使用相关功能\n{"-" * 40}\n
-    """
+\n\n
+--------------菜单----------------
+详细菜单：显示此菜单页面
+下载漫画：下载JMcomic漫画
+设置选项：设置文件路径等（开发中）
+切换主题：更改界面主题风格
+关于项目：显示关于页面
+退出程序：退出该程序
+**↑/↓ - 选择 | ENTER - 确认
+---------------------------------\n
+"""
 
     settings = f"""
-\n\n{"-" * 18}设置{"-" * 18}
-1.日志：关闭下载日志输出
-**输入数字以使用相关功能\n{"-" * 40}\n
+\n\n
+--------------设置-------------
+日志：关闭下载日志输出
+**↑/↓ - 选择 | ENTER - 确认
+------------------------------\n
 """
 
     about = f"""
-\n\n感谢您使用JMcomic Downloader！本项目由Eix0721开发
+\n\n
+感谢您使用JMcomic Downloader！
+本项目由Eix0721开发，遵循MIT开源许可证。
 仓库地址：{link.pj}
 
 本项目使用了以下第三方库：
 **JMComic Crawler Python
-描述：提供漫画下载的核心功能
+描述：提供漫画下载核心功能
 地址：{link.jm}
 **pyYAML
-描述：未来将用于配置文件支持
+描述：未来用于配置文件支持
 地址：{link.yaml}
 **InquirerPy
-描述：用于构建交互式命令行界面
+描述：构建交互式命令行界面
 地址：{link.InquirerPy}
 
 感谢以上项目的开发者与贡献者为开源社区作出的奉献！
-JMcomic 也是一款十分优秀的漫画软件，关爱禁漫娘，请不要一次性下载过多本子!
+JMcomic 也是一款十分优秀的漫画软件，
+关爱禁漫娘，请不要一次性下载过多本子!
 """
+#TODO 调整数据结构
+class infurce_style:
+    all_styles = {
+    "默认风格":{
+    "questionmark": "#e5c07b",
+    "answermark": "#e5c07b",
+    "answer": "#61afef",
+    "input": "#98c379",
+    "question": "",
+    "answered_question": "",
+    "instruction": "#abb2bf",
+    "long_instruction": "#abb2bf",
+    "pointer": "#61afef",
+    "checkbox": "#98c379",
+    "separator": "",
+    "skipped": "#5c6370",
+    "validator": "",
+    "marker": "#e5c07b",
+    "fuzzy_prompt": "#c678dd",
+    "fuzzy_info": "#abb2bf",
+    "fuzzy_border": "#4b5263",
+    "fuzzy_match": "#c678dd",
+    "spinner_pattern": "#e5c07b",
+    "spinner_text": "",
+},
+    "极简黑白":{
+    "questionmark": "fg:#ffffff bold",   # 白色 ?
+    "question":     "bold",              # 问题本身
+    "answer":       "fg:#ffffff bold",   # 已选项
+    "pointer":      "fg:#ffffff bold",   # 指针 >
+    "highlighted":  "reverse",           # 反白
+    "instruction":  "",                  # 提示文字
+},
+    "赛博朋克":{
+    "questionmark": "fg:#00ffff bold",   # 青色 ?
+    "question":     "fg:#ff00ff bold",   # 洋红问题
+    "answer":       "fg:#39ff14 bold",   # 亮绿答案
+    "pointer":      "fg:#ffaa00 bold",   # 橙黄指针
+    "highlighted":  "fg:#ff00ff bold underline",
+    "instruction":  "fg:#888888",
+},
+    "商务深蓝":{
+    "questionmark": "fg:#0052cc bold",   # IBM 蓝
+    "question":     "fg:#0052cc bold",
+    "answer":       "fg:#36b37e bold",   # 成功绿
+    "pointer":      "fg:#0052cc",
+    "highlighted":  "fg:#ffffff bg:#0052cc",
+    "instruction":  "fg:#6b778c",
+}}
