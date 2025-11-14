b51e32ae18d0b3253e9d480c1fce3d7b5f220a32
Eix0721 <2737757070@qq.com>
2025-11-14 12:28:16 +0800

    refactor & fix
    封装 InquirerPy 部分函数至 ui.py，简化调用
    修复 错误传参导致的下载漫画功能无法使用

--- src/libs/core.py
+++ src/libs/core.py
@@ -1,20 +1,19 @@
+from email import message
 import traceback
 import time
 import sys
 import re 
 import os
+import libs.ui as ui
 
-from libs import jmcomic as jm
+from libs import InquirerPy, jmcomic as jm
 from libs import yaml
-from libs import InquirerPy
-from libs.InquirerPy import inquirer,utils
 from libs.text import text, infurce_style
 
 
 
 success_initialize = False
 is_show_download_log = True
-global_style = InquirerPy.get_style (infurce_style.all_styles["默认风格"],style_override=True)
 
 
 
@@ -30,17 +29,12 @@ def initialize () ->bool:
 
 def jmcomic_download() ->None:
     global is_show_download_log,global_style
-    jm_ids = InquirerPy.inquirer.text(  # type: ignore
-        message="请输入要下载的JMcomic车号（多个车号用空格分隔）：",
-        style = global_style,
-        pointer = ">"
-    ).execute()
+    jm_ids = ui.input_text("请输入要下载的 JMcomic 车号（多个车号用空格分隔）：")
     if not re.fullmatch(r"(\d+\s*)+", jm_ids):
         print ("输入的格式有误，请输入仅包含数字的车号，多个车号用空格分隔！")
         return
     jm_ids = jm_ids.strip().split ()
-    is_permit = InquirerPy.inquirer.confirm(message=f"即将开始下载{jm_ids}，是否确认？",# type: ignore
-                                            default=True,style = global_style,pointer = ">").execute()  
+    is_permit = ui.confirm_yes_no(f"即将下载：{jm_ids}，是否继续？")
     if not is_show_download_log:
         print ("已关闭下载日志输出，若要开启，请重启程序")
         print ("下载任务已开始，请耐心等待...")
@@ -58,52 +52,19 @@ def jmcomic_download() ->None:
     else:
         print ("已取消下载任务。")
 
-def set_style() ->None:
-    global global_style
-    is_keep_going = True
-    style_choice = "默认风格"
-    while is_keep_going:
-        try:
-            style_choices = list(infurce_style.all_styles.keys()) + ["取消"]
-            style_choice = InquirerPy.inquirer.select(  # type: ignore
-                message = "请选择界面风格：",
-                choices = style_choices,
-                default = style_choice,
-                style = global_style,
-                pointer = ">"
-            ).execute()
-            if style_choice == "取消":
-                is_keep_going = False
-                return
-            style_args = infurce_style.all_styles[style_choice]
-            global_style = InquirerPy.get_style (style_args,style_override=True)
-        except Exception as err:
-            print(f"切换样式时发生错误：{type(err).__name__}:{err}")
-            break
-
 
 def get_command(option) ->str:
     global global_style
     command = ""
     if option == "menu":
-        command = InquirerPy.inquirer.select(   # type: ignore
+        command = ui.select_choice (
             message="\n请选择操作：",
-            choices=[
-                "详细菜单","下载漫画","设置选项",
-                "切换主题","关于项目","退出程序"
-            ],
-            style = global_style,
-            pointer = ">"
-        ).execute()
+            choices=["详细菜单","下载漫画","设置选项",
+                    "切换主题","关于项目","退出程序"])
     elif option == "settings":
-        command = InquirerPy.inquirer.select(  # type: ignore
-            message="请选择设置项：",
-            choices=[
-                "关闭下载日志输出","退出设置"
-            ],
-            style = global_style,
-            pointer = ">"
-        ).execute()
+        command = ui.select_choice (
+                message="请选择设置项：",
+                choices=["关闭下载日志输出","退出设置"])
     return command
 
 
@@ -126,7 +87,7 @@ def execute_command(command: str) ->None:
             else:
                 print (f"指令不存在，输入“1”以查看菜单")
     elif command == "切换主题":
-        set_style()
+        ui.set_style()
     elif command == "关于项目":
         print (text.about)
     elif command == "退出程序":
---
+++ src/libs/ui.py
@@ -0,0 +1,51 @@
+from libs import InquirerPy
+from libs.InquirerPy import inquirer,utils
+from libs.text import text, infurce_style
+global_style = InquirerPy.get_style (infurce_style.all_styles["默认风格"],style_override=True)
+
+def input_text (mesage) -> str:
+    output_str = InquirerPy.inquirer.text(  # type: ignore
+        message = mesage,
+        style = global_style,
+    ).execute()
+    return output_str
+
+def confirm_yes_no (message) -> bool:
+    choice = InquirerPy.inquirer.select(   # type: ignore
+        message=message,
+        choices=[
+            "确认","取消"
+        ],
+        style = global_style,
+        pointer = ">"
+    ).execute()
+    return choice == "确认"
+
+def select_choice (message,choices,default=None) -> str:
+    choice = InquirerPy.inquirer.select(  # type: ignore
+                message = message,
+                choices = choices,
+                default = default,
+                style = global_style,
+                pointer = ">"
+            ).execute()
+    return choice
+
+def set_style() ->None:
+    global global_style
+    is_keep_going = True
+    style_choice = "默认风格"
+    while is_keep_going:
+        try:
+            style_choices = list(infurce_style.all_styles.keys()) + ["取消"]
+            style_choice = select_choice ("请选择主题风格：",
+                                             style_choices,
+                                             default=style_choice)
+            if style_choice == "取消":
+                is_keep_going = False
+                return
+            style_args = infurce_style.all_styles[style_choice]
+            global_style = InquirerPy.get_style (style_args,style_override=True)
+        except Exception as err:
+            print(f"切换样式时发生错误：{type(err).__name__}:{err}")
+            break
